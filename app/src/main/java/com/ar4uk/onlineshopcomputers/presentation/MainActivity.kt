011c29a7ea33c3531929a05ae8c2615d47ed667f
a.galeev <a.galeev@malltech-lab.ru>
2024-09-25 18:26:00 +0500

    init

---
+++ app/src/main/java/com/ar4uk/onlineshopcomputers/presentation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ar4uk.onlineshopcomputers.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
