b225efe260f76cae8bde36441eb5427c6aa3c426
a.galeev <a.galeev@malltech-lab.ru>
2024-09-29 00:59:52 +0500

    carousel slider AsyncImagePainter

--- app/src/main/java/com/ar4uk/onlineshopcomputers/presentation/home/components/CarouselSlider.kt
+++ app/src/main/java/com/ar4uk/onlineshopcomputers/presentation/home/components/CarouselSlider.kt
@@ -1,5 +1,7 @@
 package com.ar4uk.onlineshopcomputers.presentation.home.components
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,16 +11,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -26,8 +34,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
 import com.ar4uk.onlineshopcomputers.presentation.helpers.SIDE_PADDING
 import com.ar4uk.onlineshopcomputers.presentation.ui.theme.Green
 import com.ar4uk.onlineshopcomputers.presentation.ui.theme.Grey
@@ -49,17 +62,51 @@ fun CarouselSlider(images: List<String>) {
         ),
         pageSpacing = 16.dp
     ) { index ->
-        Card(
-            modifier = Modifier.fillMaxWidth(),
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 8.dp
-            )
+        val model = ImageRequest
+            .Builder(LocalContext.current)
+            .data(images[index])
+            .size(Size.ORIGINAL)
+            .build()
+        val imageState = rememberAsyncImagePainter(model = model).state
+
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(14.dp))
+                .background(MaterialTheme.colorScheme.secondaryContainer),
         ) {
-            AsyncImage(
-                modifier = Modifier.fillMaxWidth(),
-                model = images[index],
-                contentDescription = null
-            )
+            if (imageState is AsyncImagePainter.State.Success) {
+                Image(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.Crop,
+                    painter = imageState.painter,
+                    contentDescription = null
+                )
+            }
+
+            if (imageState is AsyncImagePainter.State.Loading) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .heightIn(min = 140.dp)
+                        .size(50.dp)
+                        .align(Alignment.Center),
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            if (imageState is AsyncImagePainter.State.Error) {
+                Icon(
+                    imageVector = Icons.Rounded.Warning,
+                    contentDescription = null
+                )
+            }
+//            AsyncImage(
+//                modifier = Modifier.fillMaxWidth(),
+//                model = images[index],
+//                contentDescription = null
+//            )
         }
     }
     Spacer(modifier = Modifier.height(10.dp))
